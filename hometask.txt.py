58b731f8a02a07eaaa9c525b4a8d7b03044ce6f4
Valera <valerylarionov06@gmail.com>
2023-11-24 23:18:07 +0300

    add commits info

--- hometask.txt.py
+++ hometask.txt.py
@@ -1,4 +1,4 @@
-чем отличаются:
+2) чем отличаются:
 
 --- a/README
 +++ b/README
@@ -7,3 +7,27 @@
 +Create release branch with human (master) in boots (boots branch) and hat (hat branch)
 with nice buttons on his jacket (buttons branch)
 
+4) в репозитории 2 ветки, т.к. продолжение задания
+коммиты до отправки текста с ответом на 4 пункт:
+в ветке master:
+
+add hometask file
+Merge branch 'boots_buttons'
+Merge remote-tracking branch 'remotes/origin/hat'
+Merge remote-tracking branch 'remotes/origin/buttons' into boots_buttons
+Merge remote-tracking branch 'remotes/origin/boots' into boots_buttons
+fix readme
+dots
+README added
+boots
+heat added
+init
+
+в ветке boots_buttons:
+Merge remote-tracking branch 'remotes/origin/buttons' into boots_buttons
+Merge remote-tracking branch 'remotes/origin/boots' into boots_buttons
+fix readme
+dots
+README added
+boots
+init
