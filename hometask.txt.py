d4773e5cb3ac121e1d7d9a15be18eb8d7cbab136
Valera <valerylarionov06@gmail.com>
2023-11-24 23:02:43 +0300

    add hometask file

---
+++ hometask.txt.py
@@ -0,0 +1,9 @@
+чем отличаются:
+
+--- a/README
++++ b/README
+@@ -1,3 +1,3 @@
+-Create release branch with human (master) in boots (boots branch) and hat (hat branch).
++Create release branch with human (master) in boots (boots branch) and hat (hat branch)
+with nice buttons on his jacket (buttons branch)
+
